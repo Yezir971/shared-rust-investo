b77497146fa86d9a11247d6f786bde9af88d2575
Yezir971 <james_ahmedaly@yahoo.com>
2026-05-07 13:51:41 +0200

    feat : config deploy to docker

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,15 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub async fn run_migrations(pool: &sqlx::PgPool) -> anyhow::Result<()> {
+    sqlx::migrate!()
+        .run(pool)
+        .await?;
+        
+    Ok(())
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
